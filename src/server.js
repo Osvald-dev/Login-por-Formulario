90481b6fdd23c1f86edcfe8233d338b1f7aecc42
Osvaldo Casabella <ocasabella98@gmail.com>
2023-02-02 09:02:12 -0300

    login

---
+++ src/server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import mongoStore from 'connect-mongo';
+import {product, cart, user} from './routes/index.js';
+import session from 'express-session';
+import {engine} from 'express-handlebars';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+
+const PORT = 3027;
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static('public'));
+
+app.set('views', './src/views');
+app.set('view engine', 'hbs');
+
+app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'index.hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}))
+
+app.use(
+    session({
+        store: mongoStore.create({
+            mongoUrl: process.env.MONGO_URI,
+            options: {
+                userNewParser: true,
+                useUnifiedTopology: true,
+            }
+        }),
+        secret: process.env.SECRET,
+        resave: true,
+        saveUninitialized: true,
+        cookie: {maxAge: 600000} //10 min.
+        
+}))
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/api/productos', product);
+app.use('/api/carrito', cart);
+app.use('/api/usuario', user);
+
+const server = app.listen(PORT, () => {
+    console.log(` >>>>> 🚀 Server started at http://localhost:${PORT}`)
+    })
+    
+server.on('error', (err) => console.log(err));
